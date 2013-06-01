321a278fde782b2bbe347850f807b83cfeca92a2
Patrick Huesler <patrick.huesler@gmail.com>
2013-06-01 15:19:22 +0200

    go fmt FTW

--- dsm.go
+++ dsm.go
@@ -1,36 +1,35 @@
 package main
 
 import (
-    "net/http"
-    "fmt"
-    "io/ioutil"
-    "log"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 )
 
 func main() {
-  serverFiles()
-  fmt.Printf("%s", getFile("checksum.md5"))
+	serverFiles()
+	fmt.Printf("%s", getFile("checksum.md5"))
 }
 
 func getFile(fileName string) string {
-    res, err := http.Get("http://localhost:8080/" + fileName)
+	res, err := http.Get("http://localhost:8080/" + fileName)
 
-    if err != nil {
-      log.Fatal(err)
-    }
-    robots, err := ioutil.ReadAll(res.Body)
-    res.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	robots, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
 
-    if err != nil {
-      log.Fatal(err)
-    }
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    return string(robots)
+	return string(robots)
 
 }
 
 func serverFiles() {
-  http.Handle("/", http.FileServer(http.Dir("./tmp/")))
-  go http.ListenAndServe(":8080", nil)
+	http.Handle("/", http.FileServer(http.Dir("./tmp/")))
+	go http.ListenAndServe(":8080", nil)
 }
-
