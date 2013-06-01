f10e79e4aa9e547684d558374f937d625070c346
Patrick Huesler <patrick.huesler@gmail.com>
2013-06-01 16:26:10 +0200

    add sqlite3

--- dsm.go
+++ dsm.go
@@ -3,10 +3,12 @@ package main
 import (
 	"crypto/md5"
 	"fmt"
+  "os"
 	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
+  "github.com/kuroneko/gosqlite3"
 )
 
 func main() {
@@ -19,6 +21,7 @@ func main() {
 	writeFile("tmp/downloaded_file", data)
 
 	fmt.Printf("%s\n%s\n", expectedChecksum, actualChecksum)
+  doTheDb()
 }
 
 func getFile(fileName string) []byte {
@@ -45,4 +48,29 @@ func serverFiles() {
 
 func writeFile(path string, data []byte) {
 	ioutil.WriteFile(path, data, 0755)
+}
+
+func doTheDb(){
+  filename := "db/statistics.db"
+	db, e := sqlite3.Open(filename)
+	if e != nil {
+		log.Fatalf("Creating %v failed with error: %v", db, e)
+	}
+	if _, e = db.Execute( "CREATE TABLE foo (id INTEGER PRIMARY KEY ASC, name VARCHAR(10));" ); e != nil {
+		log.Fatalf("Create Table foo failed with error: %v", e)
+	}
+	db.Close()
+
+	if _, e := os.Stat(filename); e != nil {
+		log.Fatalf("Checking %v existence failed with error: %v", filename, e)
+	}
+
+	//	If new.db already exists and is a valid SQLite3 database this should succeed
+	if db, e = sqlite3.Open(filename); e != nil {
+		log.Fatalf("Reopening %v failed with error: %v", db, e)
+	}
+	defer db.Close()
+	if _, e = db.Execute( "INSERT INTO foo (id,name) VALUES ('1', 'John');" ); e != nil {
+		log.Fatalf("Insert into foo failed with error: %v", e)
+	}
 }
