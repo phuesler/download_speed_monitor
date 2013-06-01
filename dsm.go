e03ec9c97be07296a61c33f24366b328003d6bfe
Patrick Huesler <patrick.huesler@gmail.com>
2013-06-01 15:15:48 +0200

    server files and download them

---
+++ dsm.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+    "net/http"
+    "fmt"
+    "io/ioutil"
+    "log"
+)
+
+func main() {
+  serverFiles()
+  fmt.Printf("%s", getFile("checksum.md5"))
+}
+
+func getFile(fileName string) string {
+    res, err := http.Get("http://localhost:8080/" + fileName)
+
+    if err != nil {
+      log.Fatal(err)
+    }
+    robots, err := ioutil.ReadAll(res.Body)
+    res.Body.Close()
+
+    if err != nil {
+      log.Fatal(err)
+    }
+
+    return string(robots)
+
+}
+
+func serverFiles() {
+  http.Handle("/", http.FileServer(http.Dir("./tmp/")))
+  go http.ListenAndServe(":8080", nil)
+}
+
