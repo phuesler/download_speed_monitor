229cabd26404e696baa99f54ad5b9e564e0b17e2
Patrick Huesler <patrick.huesler@gmail.com>
2013-06-01 14:07:12 +0200

    also generate checksum of file

--- bin/generate_file.sh
+++ bin/generate_file.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
+OUTPUT=tmp/file
+
 mkdir -p tmp
-dd if=/dev/zero bs=1024 count=1010 of=tmp/file
+dd if=/dev/zero bs=1024 count=1010 of=$OUTPUT
+md5 -q $OUTPUT > tmp/checksum.md5
+ls -lah tmp/
+
